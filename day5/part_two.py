98375ea19d815db9777d153c02c2cd9ef75b0d52
ericlchen <ericlchen@utexas.edu>
2023-12-05 22:58:52 -0800

    Finished da y5 part2

--- day5/part_two.py
+++ day5/part_two.py
@@ -1,4 +1,4 @@
-import bisect
+from collections import deque
 
 def read_map(f):
     dic = {}
@@ -6,52 +6,89 @@ def read_map(f):
     f.readline()
     line = f.readline()
     while line and line != "\n":
-        parsed_line = [int(x) for x in line.strip().split()]
-        dic[parsed_line[1]] = (parsed_line[0], parsed_line[2])
+        dst_start, src_start, range_len = [int(x) for x in line.strip().split()]
+        dic[(src_start, src_start + range_len)] = dst_start - src_start
         line = f.readline()
     return dic
 
-def get_floor_key(arr, key):
-    index = bisect.bisect_right(arr, key)
-    if index:
-        return arr[index - 1]
-    return -1
-
-def get_translated_dict(input, translation_dict):
-    translation_keys = sorted([x for x in translation_dict.keys()])
-    ret = set()
-    for x in input:
-        floor_key = get_floor_key(translation_keys, x)
-        if floor_key != -1 and floor_key + translation_dict[floor_key][1] > x:
-            ret.add(x + translation_dict[floor_key][0] - floor_key)
+def merge_intervals(intervals):
+    intervals.sort()
+    merged = []
+    for interval in intervals:
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
         else:
-            ret.add(x)
-    return ret
+            merged[-1][1] = max(merged[-1][1], interval[1])
+    return merged
 
+# This took longer than I liked to figure out
+def get_translated_intervals(intervals, translation_dict):
+    ret_intervals = []
+    translation_intervals = deque(sorted(translation_dict.keys()))
+    sorted_intervals = deque(sorted(intervals))
+    
+    while sorted_intervals:
+        if not translation_intervals:
+            ret_intervals.append(sorted_intervals.popleft())
+            continue
+        # Interval start lower than translation start
+        if sorted_intervals[0][0] < translation_intervals[0][0]:
+            # No overlap, add as is
+            if sorted_intervals[0][1] <= translation_intervals[0][0]:
+                ret_intervals.append(sorted_intervals.popleft())
+            # Overlaps at end, add beginning as original, next loop takes care of rest
+            else:
+                ret_intervals.append([sorted_intervals[0][0], translation_intervals[0][0]])
+                sorted_intervals[0] = [translation_intervals[0][0], sorted_intervals[0][1]]
+        # Interval start is higher than translation start
+        else:
+            # Translation end before interval start, remove translation
+            if translation_intervals[0][1] <= sorted_intervals[0][0]:
+                translation_intervals.popleft()
+            # Translation end after interval start, overlaps in some way
+            else:
+                # Translation end after interval end, whole interval is translated
+                if translation_intervals[0][1] >= sorted_intervals[0][1]:
+                    interval = sorted_intervals.popleft()
+                    translation = translation_dict[translation_intervals[0]]
+                    ret_intervals.append([interval[0] + translation, interval[1] + translation])
+                # Translation end before interval end, translated interval added
+                else:
+                    translation = translation_dict[translation_intervals[0]]
+                    ret_intervals.append([sorted_intervals[0][0] + translation, translation_intervals[0][1] + translation])
+                    sorted_intervals[0] = [translation_intervals[0][1], sorted_intervals[0][1]]
+                    translation_intervals.popleft()
+
+    return merge_intervals(ret_intervals)
 
 with open("input.txt") as f:
     seeds = [int(x) for x in f.readline().strip().split(": ")[1].split()]
+    seed_intervals = []
+    for i in range(0, len(seeds) - 1, 2):
+        seed_intervals.append((seeds[i], seeds[i] + seeds[i + 1]))
+    seed_intervals = merge_intervals(seed_intervals)
+
     f.readline()
 
     seed_to_soil = read_map(f)
-    soil = get_translated_dict(seeds, seed_to_soil)
+    soil_intervals = get_translated_intervals(seed_intervals, seed_to_soil)
 
     soil_to_fertilizer = read_map(f)
-    fertilizer = get_translated_dict(soil, soil_to_fertilizer)
+    fertilizer_intervals = get_translated_intervals(soil_intervals, soil_to_fertilizer)
 
     fertilizer_to_water = read_map(f)
-    water = get_translated_dict(fertilizer, fertilizer_to_water)
+    water_intervals = get_translated_intervals(fertilizer_intervals, fertilizer_to_water)
 
     water_to_light = read_map(f)
-    light = get_translated_dict(water, water_to_light)
+    light_intervals = get_translated_intervals(water_intervals, water_to_light)
 
     light_to_temperature = read_map(f)
-    temperature = get_translated_dict(light, light_to_temperature)
+    temperature_intervals = get_translated_intervals(light_intervals, light_to_temperature)
 
     temperature_to_humidity = read_map(f)
-    humidity = get_translated_dict(temperature, temperature_to_humidity)
+    humidity_intervals = get_translated_intervals(temperature_intervals, temperature_to_humidity)
 
     humidity_to_location = read_map(f)
-    location = get_translated_dict(humidity, humidity_to_location)
+    location_intervals = get_translated_intervals(humidity_intervals, humidity_to_location)
 
-    print("Answer is:", min(location))
+    print("Answer is:", min(location_intervals)[0])
